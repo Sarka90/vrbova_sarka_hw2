0ff8c7b2d6801759c04ea02a82f1e0ae1fe03fc6
Sarka90 <janeckova.sa@seznam.cz>
2024-10-30 10:36:39 +0100

    Add files via upload

---
+++ vrbova_sarka_hw2.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+list = []
+
+with open("netflix_titles.tsv", mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter='\t')
+    for row in reader:
+        title = row["PRIMARYTITLE"]
+        if row["DIRECTOR"]:
+            directors = row["DIRECTOR"].split(",")
+        else:
+            directors = []
+        if row["CAST"]:
+            cast = row["CAST"].split(",")
+        else:
+            cast = []
+        if row["GENRES"]:
+            genres = row["GENRES"].split(",")
+        else:
+            genres = []
+        if row["STARTYEAR"].isdigit():
+            year = int(row["STARTYEAR"])
+            decade = (year // 10) * 10
+        else:
+            decade = None
+        movie_dictionary = {
+            "title": [director.strip() for director in directors if director.strip()],
+            "cast": [actor.strip() for actor in cast if actor.strip()],
+            "genres": [genre.strip() for genre in genres if genre.strip()],
+            "decade": decade
+        }
+        list.append(movie_dictionary)
+with open("hw02_output.json", mode='w', encoding='utf-8') as json_file:
+    json.dump(list, json_file, indent=4, ensure_ascii=False)
